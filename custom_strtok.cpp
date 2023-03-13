f8137287b65153bd3751bb307c635af0b31f5e4d
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2023-03-14 07:57:26 +1100

    strtok custom: string tokenisation

---
+++ custom_strtok.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+char *strtok_custom(char c_arr[], char delim) {
+    static char *input = NULL;
+
+    if (c_arr != NULL) input = c_arr;
+
+    if (input == NULL) return NULL;
+
+    char *token = new char[strlen(input) + 1];
+
+    int i = 0;
+    for (; input[i] != '\0'; i++) {
+        if (input[i] != delim) {
+            token[i] = input[i];
+        }
+        else {
+            token[i] = '\0';
+            input += i + 1;
+            return token;
+        }
+    }
+
+    input = NULL;
+    token[i] = '\0';
+
+    return token;
+}
+
+int main(void) {
+    char arr[] = "My name is Debashish Sarkar Durjoy";
+
+    char *c = strtok_custom(arr, ' ');
+    while (c != NULL) {
+        cout << c << endl;
+        c = strtok_custom(NULL, ' ');
+    }
+
+    return 0;
+}
