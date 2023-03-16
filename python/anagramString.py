e2edb31e9d5b6abcd9a2d711541c0fdf1431fb8f
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2023-03-16 21:59:38 +1100

    anagram indices python

---
+++ python/anagramString.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def is_anagram(s1, s2):
+    return Counter(s1) == Counter(s2)
+
+def anagram_indices(word, s):
+    result = []
+    for i in range(len(s) - len(word) + 1):
+        window = s[i: i + len(word)]
+        if is_anagram(window, word):
+            result.append(i)
+    return result
+
+
+if __name__ == "__main__":
+    print(anagram_indices("ab", "abxaba"))
